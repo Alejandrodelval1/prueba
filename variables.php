fd3123a1b88f996320d22e4b2a82bff139ae2d97
Alejandrodelval1 <gamesalesso@gmail.com>
2020-04-14 12:33:46 +0200

    2

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1> Ejemplo variables</h1>
+<?php 
+$nombre="Alex";
+echo "Hola, $nombre <br>";
+echo 'Hola,$nombre'; // diferente con comillas simple y dobles con dobles comprueba si hay referencias de variasbles o quieres concatenar
+ ?>
+ <hr>
+ <?php 
+ echo "soy $nombre <br>";
+ echo 'hola ,  '.$nombre; // el punto sirve para concatenar 
+
+  ?>
+</body>
+</html>
